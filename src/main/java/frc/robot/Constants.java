65df29ab35cb6021a653cd5f582486a1a5aec655
Justin <justin@tervay.com>
2023-01-06 17:42:26 -0500

    Add Phoenix

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,6 @@
 package frc.robot;
 
 public class Constants {
-    //in case you need a zero :)
-    public static final int zero = 0;
+  // in case you need a zero :)
+  public static final int zero = 0;
 }
