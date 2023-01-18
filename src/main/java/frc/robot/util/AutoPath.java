150c2882bf76f83adb6ad749298cd5559f04d3da
Justin <justin@tervay.com>
2023-01-17 23:03:57 -0500

    I'm not proud of this

--- src/main/java/frc/robot/util/AutoPath.java
+++ src/main/java/frc/robot/util/AutoPath.java
@@ -2,22 +2,26 @@ package frc.robot.util;
 
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+import edu.wpi.first.math.util.Units;
 
 public class AutoPath {
+  public static final double fieldLength = Units.inchesToMeters(651.25);
+  public static final double fieldWidth = Units.inchesToMeters(315.5);
+
   public enum Autos {
     PART_1("goto1stcargo"),
     PART_2("backtogrid"),
     PART_3("getonthebridge");
 
-    private String filename;
+    private PathPlannerTrajectory trajectory;
 
     private Autos(String filename) {
-      this.filename = filename;
+      this.trajectory =
+          ReflectedTransform.reflectiveTransformTrajectory(
+              PathPlanner.loadPath(filename, PathPlanner.getConstraintsFromPath(filename)));
     }
 
     public PathPlannerTrajectory getTrajectory() {
-      PathPlannerTrajectory trajectory =
-          PathPlanner.loadPath(filename, PathPlanner.getConstraintsFromPath(filename));
       return trajectory;
     }
   }
