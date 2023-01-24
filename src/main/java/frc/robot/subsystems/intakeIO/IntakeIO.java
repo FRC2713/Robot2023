25218d110a33d35b14a66ee78cdd85ccf448b786
Justin <justin@tervay.com>
2023-01-23 21:16:25 -0500

    Fix package

---
+++ src/main/java/frc/robot/subsystems/intakeIO/IntakeIO.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.intakeIO;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeIO {
+
+  @AutoLog
+  public static class IntakeInputs {
+    public double outputVoltage = 0.0;
+    public boolean isOn = false;
+    public double velocityRadsPerSecond = 0.0;
+    public double tempCelcius = 0.0;
+    public double currentAmps = 0.0;
+    public double positionRad = 0.0;
+  }
+
+  public void updateInputs(IntakeInputs inputs);
+
+  public void setVoltage(double volts);
+}
