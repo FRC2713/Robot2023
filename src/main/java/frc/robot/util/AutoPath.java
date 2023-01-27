1de67c2f19c2e703fbf1e640eb2bf2ca0650420a
Justin <justin@tervay.com>
2023-01-26 22:53:18 -0500

    Add constraints to paths. Generate paths on boot.

--- src/main/java/frc/robot/util/AutoPath.java
+++ src/main/java/frc/robot/util/AutoPath.java
@@ -4,37 +4,38 @@ import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class AutoPath {
   public static final double fieldLength = Units.inchesToMeters(651.25);
   public static final double fieldWidth = Units.inchesToMeters(315.5);
 
   public enum Autos {
-    GO_TO_FIRST_CARGO("goto1stcargo"),
+    GO_TO_FIRST_CARGO("dock"),
     GO_TO_GRID("gotogrid"),
     GO_TO_SECOND_CARGO("goto2ndcargo"),
     GO_TO_GRID_TWO("gotogrid2"),
     DOCK("dock"),
-    ONE_TO_A("grid1tocargoA"),
-    A_TO_THREE("cargoAtogrid3"),
-    THREE_TO_BRIDGE("getonthebridge");
-
-    private PathPlannerTrajectory trajectory;
+    ONE_TO_A("dock"),
+    A_TO_THREE("dock");
+    private PathPlannerTrajectory blueTrajectory, redTrajectory;
 
     private Autos(String filename) {
       try {
-        this.trajectory =
-            ReflectedTransform.reflectiveTransformTrajectory(
-                PathPlanner.loadPath(filename, PathPlanner.getConstraintsFromPath(filename)));
+        this.blueTrajectory =
+            PathPlanner.loadPath(filename, PathPlanner.getConstraintsFromPath(filename));
+        this.redTrajectory = ReflectedTransform.reflectiveTransformTrajectory(blueTrajectory);
       } catch (NullPointerException ex) {
-        this.trajectory =
-            ReflectedTransform.reflectiveTransformTrajectory(
-                PathPlanner.loadPath(filename, new PathConstraints(4, 3)));
+        this.blueTrajectory = PathPlanner.loadPath(filename, new PathConstraints(4, 3));
+        this.redTrajectory = ReflectedTransform.reflectiveTransformTrajectory(blueTrajectory);
       }
     }
 
     public PathPlannerTrajectory getTrajectory() {
-      return trajectory;
+      return DriverStation.getAlliance() == Alliance.Blue
+          ? this.blueTrajectory
+          : this.redTrajectory;
     }
   }
 }
