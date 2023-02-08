4c9a093df365c662ca387cffe456ac929502bd72
sammyredha <sammy.mounsif@melroseschools.com>
2023-02-08 18:27:09 -0500

    formatting, again, plus put both extend and retracting to driver controller

--- src/main/java/frc/robot/subsystems/fourBarIO/FourBar.java
+++ src/main/java/frc/robot/subsystems/fourBarIO/FourBar.java
@@ -17,95 +17,95 @@ import org.littletonrobotics.junction.Logger;
 
 public class FourBar extends SubsystemBase {
 
-    private final ProfiledPIDController controller;
-    private final FourBarInputsAutoLogged inputs;
-    private final FourBarIO IO;
-    private double targetDegs = 0;
-    private final ArmFeedforward ff;
+  private final ProfiledPIDController controller;
+  private final FourBarInputsAutoLogged inputs;
+  private final FourBarIO IO;
+  private double targetDegs = 0;
+  private final ArmFeedforward ff;
 
-    public FourBar(FourBarIO IO) {
-        this.ff = Constants.FourBarConstants.FOUR_BAR_GAINS.createArmFeedforward();
-        this.controller =
-                Constants.FourBarConstants.FOUR_BAR_GAINS.createProfiledPIDController(
-                        new Constraints(
-                                Constants.FourBarConstants.MAX_VELOCITY,
-                                Constants.FourBarConstants.MAX_ACCELERATION));
-        this.inputs = new FourBarInputsAutoLogged();
-        IO.updateInputs(inputs);
-        this.IO = IO;
+  public FourBar(FourBarIO IO) {
+    this.ff = Constants.FourBarConstants.FOUR_BAR_GAINS.createArmFeedforward();
+    this.controller =
+        Constants.FourBarConstants.FOUR_BAR_GAINS.createProfiledPIDController(
+            new Constraints(
+                Constants.FourBarConstants.MAX_VELOCITY,
+                Constants.FourBarConstants.MAX_ACCELERATION));
+    this.inputs = new FourBarInputsAutoLogged();
+    IO.updateInputs(inputs);
+    this.IO = IO;
+  }
+
+  public void setAngleDeg(double targetDegs) {
+    double rads = Units.degreesToRadians(targetDegs);
+    if (rads < Constants.FourBarConstants.MIN_ANGLE_RADIANS
+        || rads > Constants.FourBarConstants.MAX_ANGLE_RADIANS) {
+      RedHawkUtil.ErrHandler.getInstance().addError("4Bar: Set to degress out of limits range!");
+    }
+    this.targetDegs = targetDegs;
+  }
+
+  public boolean isAtTarget() {
+    return Math.abs(inputs.angleDegreesOne - targetDegs) < 0.1;
+  }
+
+  public double getCurrentDegs() {
+    return inputs.angleDegreesOne;
+  }
+
+  public void periodic() {
+    double effort = controller.calculate(inputs.angleDegreesOne, targetDegs);
+    double ffEffort = ff.calculate(inputs.angleDegreesOne, inputs.velocityDegreesPerSecondOne);
+    effort += ffEffort;
+    effort = MathUtil.clamp(effort, -12, 12);
+
+    if ((inputs.angleDegreesOne
+        > Units.radiansToDegrees(Constants.FourBarConstants.MAX_ANGLE_RADIANS))) {
+      effort =
+          MathUtil.clamp(
+              controller.calculate(
+                  inputs.angleDegreesOne,
+                  Units.radiansToDegrees(Constants.FourBarConstants.MAX_ANGLE_RADIANS)),
+              -0.5,
+              0.5);
+      RedHawkUtil.ErrHandler.getInstance().addError("4BAR PAST MAX LIMITS!");
+    } else if (inputs.angleDegreesOne
+        < Units.radiansToDegrees(Constants.FourBarConstants.MIN_ANGLE_RADIANS)) {
+      effort =
+          MathUtil.clamp(
+              controller.calculate(
+                  inputs.angleDegreesOne,
+                  Units.radiansToDegrees(Constants.FourBarConstants.MIN_ANGLE_RADIANS)),
+              -0.5,
+              0.5);
+      RedHawkUtil.ErrHandler.getInstance().addError("4BAR PAST MIN LIMITS!");
     }
 
-    public void setAngleDeg(double targetDegs) {
-        double rads = Units.degreesToRadians(targetDegs);
-        if (rads < Constants.FourBarConstants.MIN_ANGLE_RADIANS
-                || rads > Constants.FourBarConstants.MAX_ANGLE_RADIANS) {
-            RedHawkUtil.ErrHandler.getInstance().addError("4Bar: Set to degress out of limits range!");
-        }
-        this.targetDegs = targetDegs;
+    IO.updateInputs(inputs);
+    IO.setVoltage(effort);
+
+    Logger.getInstance().recordOutput("4Bar/Target Degs", targetDegs);
+    Logger.getInstance().recordOutput("4Bar/Control Effort", effort);
+    Logger.getInstance().recordOutput("4Bar/FF Effort", ffEffort);
+    Logger.getInstance().recordOutput("4Bar/atTarget", isAtTarget());
+
+    Logger.getInstance().processInputs("4Bar", inputs);
+  }
+
+  public static class Commands {
+    public static Command setToAngle(double angleDeg) {
+      return new InstantCommand(() -> Robot.fourBar.setAngleDeg(angleDeg), fourBar);
     }
 
-    public boolean isAtTarget() {
-        return Math.abs(inputs.angleDegreesOne - targetDegs) < 0.1;
+    public static Command setAngleDegAndWait(double targetDegs) {
+      return setToAngle(targetDegs).repeatedly().until(() -> Robot.fourBar.isAtTarget());
     }
 
-    public double getCurrentDegs() {
-        return inputs.angleDegreesOne;
+    public static Command retract() {
+      return setToAngle(Units.radiansToDegrees(Constants.FourBarConstants.MAX_ANGLE_RADIANS));
     }
 
-    public void periodic() {
-        double effort = controller.calculate(inputs.angleDegreesOne, targetDegs);
-        double ffEffort = ff.calculate(inputs.angleDegreesOne, inputs.velocityDegreesPerSecondOne);
-        effort += ffEffort;
-        effort = MathUtil.clamp(effort, -12, 12);
-
-        if ((inputs.angleDegreesOne
-                > Units.radiansToDegrees(Constants.FourBarConstants.MAX_ANGLE_RADIANS))) {
-            effort =
-                    MathUtil.clamp(
-                            controller.calculate(
-                                    inputs.angleDegreesOne,
-                                    Units.radiansToDegrees(Constants.FourBarConstants.MAX_ANGLE_RADIANS)),
-                            -0.5,
-                            0.5);
-            RedHawkUtil.ErrHandler.getInstance().addError("4BAR PAST MAX LIMITS!");
-        } else if (inputs.angleDegreesOne
-                < Units.radiansToDegrees(Constants.FourBarConstants.MIN_ANGLE_RADIANS)) {
-            effort =
-                    MathUtil.clamp(
-                            controller.calculate(
-                                    inputs.angleDegreesOne,
-                                    Units.radiansToDegrees(Constants.FourBarConstants.MIN_ANGLE_RADIANS)),
-                            -0.5,
-                            0.5);
-            RedHawkUtil.ErrHandler.getInstance().addError("4BAR PAST MIN LIMITS!");
-        }
-
-        IO.updateInputs(inputs);
-        IO.setVoltage(effort);
-
-        Logger.getInstance().recordOutput("4Bar/Target Degs", targetDegs);
-        Logger.getInstance().recordOutput("4Bar/Control Effort", effort);
-        Logger.getInstance().recordOutput("4Bar/FF Effort", ffEffort);
-        Logger.getInstance().recordOutput("4Bar/atTarget", isAtTarget());
-
-        Logger.getInstance().processInputs("4Bar", inputs);
+    public static Command extend() {
+      return setToAngle(Units.radiansToDegrees(Constants.FourBarConstants.MIN_ANGLE_RADIANS));
     }
-
-    public static class Commands {
-        public static Command setToAngle(double angleDeg) {
-            return new InstantCommand(() -> Robot.fourBar.setAngleDeg(angleDeg), fourBar);
-        }
-
-        public static Command setAngleDegAndWait(double targetDegs) {
-            return setToAngle(targetDegs).repeatedly().until(() -> Robot.fourBar.isAtTarget());
-        }
-
-        public static Command retract() {
-            return setToAngle(Units.radiansToDegrees(Constants.FourBarConstants.MAX_ANGLE_RADIANS));
-        }
-
-        public static Command extend() {
-            return setToAngle(Units.radiansToDegrees(Constants.FourBarConstants.MIN_ANGLE_RADIANS));
-        }
-    }
+  }
 }
