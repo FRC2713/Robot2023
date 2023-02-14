01976f292fb3777bf30f6c077a5008d8e5e85720
sammyredha <sammy.mounsif@melroseschools.com>
2023-02-13 19:30:38 -0500

    updated our only auto command group

--- src/main/java/frc/robot/commands/fullRoutines/OneToAToThreeToBridge.java
+++ src/main/java/frc/robot/commands/fullRoutines/OneToAToThreeToBridge.java
@@ -3,37 +3,43 @@ package frc.robot.commands.fullRoutines;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.commands.GetOnBridge;
 import frc.robot.subsystems.elevatorIO.Elevator;
 import frc.robot.subsystems.fourBarIO.FourBar;
+import frc.robot.subsystems.intakeIO.Intake;
 import frc.robot.subsystems.swerveIO.SwerveSubsystem;
 import frc.robot.util.AutoPath.Autos;
 
 public class OneToAToThreeToBridge extends SequentialCommandGroup {
   public OneToAToThreeToBridge() {
     addCommands(
-        Elevator.Commands.setToHeight(30),
-        FourBar.Commands.setToAngle(Constants.FourBarConstants.RETRACTED_ANGLE_RADIANS),
         new InstantCommand(
             () -> {
               Robot.swerveDrive.resetOdometry(
                   Autos.ONE_TO_A.getTrajectory().getInitialHolonomicPose());
             }),
-        new WaitUntilCommand(() -> Robot.elevator.atTargetHeight()),
+        Elevator.Commands.setTargetHeightAndWait(
+            Constants.ElevatorConstants.ELEVATOR_CONE_HIGH_SCORE),
+        FourBar.Commands.extend(),
+        Intake.Commands.setRollerVelocityRPM(100),
+        Intake.Commands.setWheelVelocityRPM(100),
+        new WaitCommand(1),
         new ParallelCommandGroup(
             SwerveSubsystem.Commands.stringTrajectoriesTogether(Autos.ONE_TO_A.getTrajectory()),
-            Elevator.Commands.setToHeight(Constants.zero),
-            FourBar.Commands.setToAngle(Constants.FourBarConstants.EXTENDED_ANGLE_RADIANS)),
-        new ParallelCommandGroup(
-            SwerveSubsystem.Commands.stringTrajectoriesTogether(Autos.A_TO_FIVE.getTrajectory()),
-            Elevator.Commands.setToHeight(30),
-            FourBar.Commands.setToAngle(Constants.FourBarConstants.RETRACTED_ANGLE_RADIANS)),
-        Elevator.Commands.setToHeight(Constants.zero),
-        FourBar.Commands.setToAngle(Constants.FourBarConstants.EXTENDED_ANGLE_RADIANS),
-        new WaitUntilCommand(() -> Robot.elevator.atTargetHeight()),
+            Elevator.Commands.setTargetHeightAndWait(Constants.zero),
+            FourBar.Commands.retract()),
+        SwerveSubsystem.Commands.stringTrajectoriesTogether(Autos.A_TO_FIVE.getTrajectory()),
+        Elevator.Commands.setTargetHeightAndWait(
+            Constants.ElevatorConstants.ELEVATOR_CONE_HIGH_SCORE),
+        FourBar.Commands.extend(),
+        Intake.Commands.setRollerVelocityRPM(100),
+        Intake.Commands.setWheelVelocityRPM(100),
+        new WaitCommand(1),
+        FourBar.Commands.retract(),
+        Elevator.Commands.setTargetHeightAndWait(Constants.zero),
         new GetOnBridge());
   }
 }
