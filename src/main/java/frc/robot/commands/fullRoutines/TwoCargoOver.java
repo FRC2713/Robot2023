a2321c19ce021877208c946673021a161b44f60d
sammyredha <sammy.mounsif@melroseschools.com>
2023-02-14 18:48:22 -0500

    tweaked auto, added a second routine over the cable protector

--- src/main/java/frc/robot/commands/fullRoutines/TwoCargoOver.java
+++ src/main/java/frc/robot/commands/fullRoutines/TwoCargoOver.java
@@ -1,9 +1,9 @@
 package frc.robot.commands.fullRoutines;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.commands.GetOnBridge;
@@ -12,6 +12,7 @@ import frc.robot.subsystems.fourBarIO.FourBar;
 import frc.robot.subsystems.intakeIO.Intake;
 import frc.robot.subsystems.swerveIO.SwerveSubsystem;
 import frc.robot.util.AutoPath.Autos;
+import frc.robot.util.TrajectoryController;
 
 public class TwoCargoOver extends SequentialCommandGroup {
   public TwoCargoOver() {
@@ -22,19 +23,24 @@ public class TwoCargoOver extends SequentialCommandGroup {
                   Autos.ONE_TO_A.getTrajectory().getInitialHolonomicPose());
             }),
         FourBar.Commands.retract(),
-        Elevator.Commands.setTargetHeightAndWait(
-            Constants.ElevatorConstants.ELEVATOR_CONE_HIGH_SCORE),
+        Elevator.Commands.elevatorConeHighScoreAndWait(),
         FourBar.Commands.extend(),
         Intake.Commands.setRollerVelocityRPM(100),
         Intake.Commands.setWheelVelocityRPM(100),
         new WaitCommand(1),
-        new ParallelCommandGroup(
-            SwerveSubsystem.Commands.stringTrajectoriesTogether(Autos.ONE_TO_A.getTrajectory()),
-            Elevator.Commands.setTargetHeightAndWait(Constants.zero),
-            FourBar.Commands.retract()),
+        Elevator.Commands.elevatorConeFloorUpIntakeAndWait(),
+        SwerveSubsystem.Commands.stringTrajectoriesTogether(Autos.ONE_TO_A.getTrajectory()),
+        new WaitUntilCommand(() -> TrajectoryController.getInstance().isFinished()),
+        FourBar.Commands.extend(),
+        Intake.Commands.setRollerVelocityRPM(-100),
+        Intake.Commands.setWheelVelocityRPM(-100),
+        new WaitCommand(1),
+        FourBar.Commands.retract(),
+        Intake.Commands.setRollerVelocityRPM(0),
+        Intake.Commands.setWheelVelocityRPM(0),
         SwerveSubsystem.Commands.stringTrajectoriesTogether(Autos.A_TO_FIVE.getTrajectory()),
-        Elevator.Commands.setTargetHeightAndWait(
-            Constants.ElevatorConstants.ELEVATOR_CONE_HIGH_SCORE),
+        new WaitUntilCommand(() -> TrajectoryController.getInstance().isFinished()),
+        Elevator.Commands.elevatorConeHighScoreAndWait(),
         FourBar.Commands.extend(),
         Intake.Commands.setRollerVelocityRPM(100),
         Intake.Commands.setWheelVelocityRPM(100),
