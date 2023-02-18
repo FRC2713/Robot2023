233a3360d1af6450923c7e6416b3e6fabc17056c
RHR Driver Station <justin+driverstation@tervay.com>
2023-02-18 10:29:58 -0500

    Elevator tuned

--- src/main/java/frc/robot/subsystems/elevatorIO/ElevatorIOSparks.java
+++ src/main/java/frc/robot/subsystems/elevatorIO/ElevatorIOSparks.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.elevatorIO;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.RobotController;
 import frc.robot.Constants;
 
 public class ElevatorIOSparks implements ElevatorIO {
@@ -11,10 +12,12 @@ public class ElevatorIOSparks implements ElevatorIO {
   public ElevatorIOSparks() {
     left = new CANSparkMax(Constants.RobotMap.ELEVATOR_LEFT_CANID, MotorType.kBrushless);
     right = new CANSparkMax(Constants.RobotMap.ELEVATOR_RIGHT_CANID, MotorType.kBrushless);
-    left.setIdleMode(CANSparkMax.IdleMode.kBrake);
-    right.setIdleMode(CANSparkMax.IdleMode.kBrake);
+
     left.restoreFactoryDefaults();
     right.restoreFactoryDefaults();
+
+    left.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    right.setIdleMode(CANSparkMax.IdleMode.kBrake);
     left.setInverted(true);
     right.setInverted(false); // might be reversed, idk
     left.setSmartCurrentLimit(Constants.ElevatorConstants.ELEVATOR_CURRENT_LIMIT);
@@ -33,19 +36,26 @@ public class ElevatorIOSparks implements ElevatorIO {
         .getEncoder()
         .setVelocityConversionFactor(
             Constants.ElevatorConstants.ELEVATOR_VELOCITY_CONVERSION_FACTOR);
+
+    left.burnFlash();
+    right.burnFlash();
   }
 
   @Override
   public void updateInputs(ElevatorInputs inputs) {
-    inputs.outputVoltageLeft = MathUtil.clamp(left.getOutputCurrent(), -12.0, 12.0);
+    inputs.outputVoltageLeft =
+        MathUtil.clamp(left.getAppliedOutput() * RobotController.getBatteryVoltage(), -12.0, 12.0);
     inputs.heightInchesLeft = left.getEncoder().getPosition();
     inputs.velocityInchesPerSecondLeft = left.getEncoder().getVelocity();
     inputs.tempCelsiusLeft = left.getMotorTemperature();
+    inputs.currentDrawAmpsLeft = left.getOutputCurrent();
 
-    inputs.outputVoltageRight = MathUtil.clamp(right.getOutputCurrent(), -12.0, 12.0);
+    inputs.outputVoltageRight =
+        MathUtil.clamp(right.getAppliedOutput() * RobotController.getBatteryVoltage(), -12.0, 12.0);
     inputs.heightInchesRight = right.getEncoder().getPosition();
     inputs.velocityInchesPerSecondRight = right.getEncoder().getVelocity();
     inputs.tempCelsiusRight = right.getMotorTemperature();
+    inputs.currentDrawAmpsRight = right.getOutputCurrent();
   }
 
   @Override
