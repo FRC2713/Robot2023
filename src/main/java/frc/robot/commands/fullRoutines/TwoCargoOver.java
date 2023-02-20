0612f30d42cf84b3017afae3c017e793ef9bbd0e
sammyredha <sammy.mounsif@melroseschools.com>
2023-02-20 14:54:15 -0500

    started refactoring autos

--- src/main/java/frc/robot/commands/fullRoutines/TwoCargoOver.java
+++ src/main/java/frc/robot/commands/fullRoutines/TwoCargoOver.java
@@ -15,6 +15,24 @@ import frc.robot.util.AutoPath.Autos;
 import frc.robot.util.TrajectoryController;
 
 public class TwoCargoOver extends SequentialCommandGroup {
+
+  private SequentialCommandGroup score() {
+    return new SequentialCommandGroup(
+        FourBar.Commands.extend(),
+        Intake.Commands.setRollerVelocityRPM(Constants.IntakeConstants.ROLLERS_CONE_SCORE_RPM),
+        Intake.Commands.setWheelVelocityRPM(Constants.IntakeConstants.WHEELS_CONE_SCORE_RPM),
+        new WaitCommand(1));
+  }
+
+  private SequentialCommandGroup intake() {
+    return new SequentialCommandGroup(
+        FourBar.Commands.extend(),
+        Intake.Commands.setRollerVelocityRPM(
+            Constants.IntakeConstants.ROLLERS_CONE_UPRIGHT_INTAKE_RPM),
+        Intake.Commands.setWheelVelocityRPM(
+            Constants.IntakeConstants.WHEELS_CONE_UPRIGHT_INTAKE_RPM));
+  }
+
   public TwoCargoOver() {
     addCommands(
         new InstantCommand(
@@ -24,17 +42,11 @@ public class TwoCargoOver extends SequentialCommandGroup {
             }),
         FourBar.Commands.retract(),
         Elevator.Commands.elevatorConeHighScoreAndWait(),
-        FourBar.Commands.extend(),
-        Intake.Commands.setRollerVelocityRPM(100),
-        Intake.Commands.setWheelVelocityRPM(100),
-        new WaitCommand(1),
+        score(),
         Elevator.Commands.elevatorConeFloorUpIntakeAndWait(),
         SwerveSubsystem.Commands.stringTrajectoriesTogether(Autos.ONE_TO_A.getTrajectory()),
+        intake(),
         new WaitUntilCommand(() -> TrajectoryController.getInstance().isFinished()),
-        FourBar.Commands.extend(),
-        Intake.Commands.setRollerVelocityRPM(-100),
-        Intake.Commands.setWheelVelocityRPM(-100),
-        new WaitCommand(1),
         FourBar.Commands.retract(),
         Intake.Commands.setRollerVelocityRPM(0),
         Intake.Commands.setWheelVelocityRPM(0),
