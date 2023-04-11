8b40695f0bf6bf66d21ec658b82d0e812357c227
RHR Driver Station <justin+driverstation@tervay.com>
2023-04-11 19:58:13 -0400

    Automated commit at 04/11/2023 19:58:12

--- src/main/java/frc/robot/commands/PIDOnBridgeExperimental.java
+++ src/main/java/frc/robot/commands/PIDOnBridgeExperimental.java
@@ -8,7 +8,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -35,15 +34,15 @@ public class PIDOnBridgeExperimental extends SequentialCommandGroup {
       if (Math.signum(prevError) != Math.signum(currentError)) {
         speed *= .9;
       }
+      lastMeasurement = measurement;
       var out = limiter.calculate(speed);
-      if (currentError > tolerance && rollSpeed < 0.8) {
+      if (currentError > tolerance && rollSpeed < 0.75) {
         prevError = currentError;
         return -out;
-      } else if (currentError < -tolerance && rollSpeed < 0.8) {
+      } else if (currentError < -tolerance && rollSpeed < 0.75) {
         prevError = currentError;
         return out;
       }
-      lastMeasurement = measurement;
       return 0;
     }
   }
