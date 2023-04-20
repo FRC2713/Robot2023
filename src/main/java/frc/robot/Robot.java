63d862043ba551dbfb9cf3196aa9f8fa6d2aabcd
RHR Driver Station <justin+driverstation@tervay.com>
2023-04-20 08:56:12 -0400

    Automated commit at 04/20/2023 08:56:12

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -176,7 +176,7 @@ public class Robot extends LoggedRobot {
 
     Logger.getInstance().start();
 
-    // fourBar = new FourBar(isSimulation() ? new FourBarIOSim() : new FourBarIOSparks());
+    fourBar = new FourBar(isSimulation() ? new FourBarIOSim() : new FourBarIOSparks());
     elevator = new Elevator(isSimulation() ? new ElevatorIOSim() : new ElevatorIOSparks());
     intake = new Intake(isSimulation() ? new IntakeIOSim() : new IntakeIOSparks());
     vision =
@@ -186,7 +186,7 @@ public class Robot extends LoggedRobot {
     lights = new LightStrip();
     // slapper = new Slapper(true ? new SlapperIOSim() : new SlapperIOSparks());
 
-    fourBar = new FourBar(true ? new FourBarIOSim() : new FourBarIOSparks());
+    // fourBar = new FourBar(true ? new FourBarIOSim() : new FourBarIOSparks());
     // elevator = new Elevator(true ? new ElevatorIOSim() : new ElevatorIOSparks());
     // intake = new Intake(true ? new IntakeIOSim() : new IntakeIOSparks());
     // vision = new Vision(true ? new VisionIOSim() : new VisionLimelight());
