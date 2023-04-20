714dc39652ff8d44a5be937ed032649d133d9a2d
RHR Driver Station <justin+driverstation@tervay.com>
2023-04-20 18:29:25 -0400

    Automated commit at 04/20/2023 18:29:25

--- src/main/java/frc/robot/commands/fullRoutines/MobilityBridgePathing.java
+++ src/main/java/frc/robot/commands/fullRoutines/MobilityBridgePathing.java
@@ -125,7 +125,7 @@ public class MobilityBridgePathing extends SequentialCommandGroup {
 
             //       ,
             //   Commands.sequence(Commands.waitSeconds(0.5), Slapper.Commands.comeBackHome())
-            )
-        //new PIDOnBridgeExperimental(false));
+            ),
+        new PIDOnBridgeExperimental(false));
   }
 }
