27eaf846f504c6fd8f40c15b384e53ec721c1c63
Justin <justin@tervay.com>
2023-08-12 12:27:27 -0400

    accel calc

--- src/main/java/frc/robot/subsystems/elevatorIO/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevatorIO/Elevator.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
@@ -13,6 +14,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.Robot.GamePieceMode;
+import frc.robot.util.AccelerationCalc;
 import frc.robot.util.LoggableMotor;
 import frc.robot.util.RedHawkUtil;
 import frc.robot.util.SuperstructureConfig;
@@ -27,6 +29,7 @@ public class Elevator extends SubsystemBase {
   public boolean manualControl = false;
   private final ElevatorFeedforward feedforward;
   private LoggableMotor leftMotor, rightMotor;
+  private AccelerationCalc accelCalc;
 
   public Elevator(ElevatorIO IO) {
     this.feedforward = Constants.ElevatorConstants.ELEVATOR_GAINS.createElevatorFeedforward();
@@ -39,6 +42,7 @@ public class Elevator extends SubsystemBase {
     this.IO = IO;
     leftMotor = new LoggableMotor("Elevator Left", DCMotor.getNEO(1));
     rightMotor = new LoggableMotor("Elevator Right", DCMotor.getNEO(1));
+    accelCalc = new AccelerationCalc(5);
   }
 
   public void setTargetHeight(double targetHeightInches) {
@@ -77,6 +81,10 @@ public class Elevator extends SubsystemBase {
     IO.updateInputs(inputs);
     leftMotor.log(inputs.currentDrawAmpsLeft, inputs.outputVoltageLeft);
     rightMotor.log(inputs.currentDrawAmpsRight, inputs.outputVoltageRight);
+    Logger.getInstance()
+        .recordOutput(
+            "Elevator/Acceleration",
+            accelCalc.calculate(inputs.velocityInchesPerSecondLeft, Timer.getFPGATimestamp()));
 
     Logger.getInstance()
         .recordOutput("Elevator/Left is NaN", Double.isNaN(inputs.heightInchesLeft));
